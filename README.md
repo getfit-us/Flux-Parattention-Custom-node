73d63689ee642e39cad1ecd69aeccc8d67836819
Chris Scott <chris@getfit.us>
2025-01-04 14:06:14 -0500

    ### docs: update README.md with installation instructions

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Flux-Parattention-Custom-node
+
+Custom node for Flux Parattention from [ParaAttention](https://github.com/chengzeyi/ParaAttention).
+
+## Installation
+
+1. Clone the repository
+
+Place the node in the `nodes` folder.
+
+2. Restart ComfyUI
+
+3. Add the node to your workflow.
+
+4. Select the number of GPUs you want to use in parallel.
---
+++ nodes.py
@@ -0,0 +1,19 @@
+
+
+
+
+class FluxParattentionNode:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "enable": ("BOOLEAN", {"default": True}),
+                "num_gpus": ("INT", {"default": 1, "min": 1, "max": 8, "step": 1}),
+            }
+        }
+
+    def run(self, enable, num_gpus):
+        if enable:
+            return {"num_gpus": num_gpus}
+        else:
+            return None
